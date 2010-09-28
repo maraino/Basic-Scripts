942f4bce5ae9aea9c1a6011351f934de16c6f5f6
Mariano Cano <mariano.cano@gmail.com>
2010-09-27 23:05:28 -0700

    Add license information.

--- rssd/rssd.pl
+++ rssd/rssd.pl
@@ -1,4 +1,21 @@
 #!/usr/bin/perl -w
+#
+# RSSd RSS downloader. Performs automatic commands with each update.
+# Copyright (C) 2010    Mariano Cano
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 use strict;
 package main;
 use Getopt::Long;
