237ad2aca3ed9a861e16bc29d62ccb7a6456d327
Mariano Cano <mariano.cano@gmail.com>
2010-09-27 22:57:00 -0700

    Move RSS downloader (RSSd) to an OO implementation. Add new options to the RSSd:  - create configuration files  - edit configuration files

--- rssd/rssd.pl
+++ rssd/rssd.pl
@@ -1,38 +1,291 @@
 #!/usr/bin/perl -w
 use strict;
-use HTTP::Date;
+package main;
 use Getopt::Long;
-use Fcntl qw(:flock);
+
+sub main {
+    my $conf_file = $ENV{HOME} . '/.rssd.yml';
+    my $test = 0;
+    my $debug = 0;
+    my ($new, $help);
+    my ($add, $name, $url, $params);
+    
+    GetOptions (
+                # Configuration file
+                'c|conf=s' => \$conf_file,
+                
+                # Help
+                'h|help' => \$help,
+                
+                # New file
+                'n|new' => \$new, 
+
+                # Add link
+                'a|add'      => \$add, 
+                'name=s'   => \$name,
+                'url=s'   => \$url,
+                'params=s' => \$params,
+                
+                # Extra flags
+                't|test'  => \$test,
+                'd|debug' => \$debug);
+
+    if ($help) {
+        help();
+        return;
+    }
+    
+    my $rssd;
+    if ($new) {
+        $rssd = RSSd->new();
+    } else {
+        $rssd = RSSd->new($conf_file);
+    }
+    
+    # Set attributes
+    if ($test) {
+        $rssd->test(1);
+    }
+
+    if ($debug) {
+        $rssd->debug(1);
+    }
+    
+    # Run operations
+    if ($new) {
+        $rssd->create($conf_file);
+        $rssd->save();
+    } elsif($add) {
+        my @p;
+        my @add_params = split(',', $params);
+        $params = {};
+        foreach (@add_params) {
+            @p = split('=', $_);
+            $params->{$p[0]} = $p[1];
+        }
+        $rssd->add($name, $url, $params);
+        $rssd->save();
+    } else {
+        $rssd->run();
+        $rssd->save();
+    }
+
+    # my $lock_file = $conf_file . '.lck';
+    # open (my $fl, ">> $lock_file") or die ("Error: cannot open $lock_file");
+    # Fcntl::flock($fl, LOCK_EX) or die ("Error: cannot lock $lock_file");
+    
+    # unlink($lock_file);
+    # Fcntl::flock($fl, LOCK_UN) or die ("Error: cannot unlock $lock_file");
+    # close($fl);
+}
+
+sub help {
+    print <<USAGE;
+Usage: $0 [--run] [--conf config_file] [OPTIONS]
+       $0 --new [--conf config_file] [OPTIONS]
+       $0 --add --name Name --url http://example.com/rss.xml [--params last=DATE,command=COMMAND,path=PATH] [OPTIONS]
+       $0 --help 
+Options: 
+  -t | --test     Don't perform any action.
+  -d | --debug    Show debug messages.
+
+Commands: 
+  -r | --run      Default action. Get latest RSS and performa actions.
+  -n | --new      Create a new configuration YAML file.
+  -a | --add      Add a RSS feed to the selected YAML file.
+  -h | --help     Show this help summary page.
+
+Default YAML file: ~/.rssd.yml
+
+USAGE
+}
+
+main();
+
+package RSSd;
+use Carp;
+use HTTP::Date;
 use YAML;
 use XML::RSS::Parser;
 
-my $DEBUG = 1;
+sub new {
+    my $class      = shift;
+    my $self       = {};
+    bless ($self, $class);
+    
+    $self->{CONF}  = undef;
+    $self->{DEBUG} = undef;
+    $self->{TEST}  = undef;
+    $self->{YAML}  = undef;
 
-sub read_config($) {
-    my $file = shift;
+    if (scalar(@_) > 0) {
+        $self->{CONF} = shift;
+        $self->_read_config();
+    }
+    
+    return $self;
+}
 
-    return undef if (!-e $file);
+sub test {
+    my $self = shift;
+    $self->{TEST} = shift if @_;
+    return $self->{TEST};
+}
 
-    my $conf = YAML::LoadFile($file);
+sub debug {
+    my $self = shift;
+    $self->{DEBUG} = shift if @_;
+    return $self->{DEBUG};
+}
+
+sub conf {
+    my $self = shift;
+    $self->{CONF} = shift if @_;
+    return $self->{CONF};
+}
+
+sub create($$) {
+    my ($self, $conf) = @_;
+    
+    $self->{CONF} = $conf;
+    $self->{YAML} = {
+                     default => {
+                                 command => undef, 
+                                 path => undef,
+                                }, 
+                     rss => undef
+                    };
+}
+
+sub add($$$$) {
+    my ($self, $name, $url, $params) = @_;
+    
+    $self->_read_config() if(!defined($self->{YAML}));
+    
+    if (!exists($self->{YAML}->{rss})) {
+        $self->{YAML}->{rss} = {};
+    }
+    
+    if (!exists($self->{YAML}->{rss}->{$name})) {
+        $self->{YAML}->{rss}->{$name} = {};
+    }
+
+    $self->{YAML}->{rss}->{$name}->{url} = $url;
+    
+    # Add extra parameters like last,command or path
+    while(my ($k, $v) = each(%$params)) {
+        $self->{YAML}->{rss}->{$name}->{$k} = $v;
+    }
+}
+
+sub save {
+    my ($self) = @_;
+
+    if (!defined($self->{CONF})) {
+        croak("configuration file is not defined");
+    }
+    
+    if (!defined($self->{YAML})) {
+        croak("YAML has not been created");
+    }
+    
+    if (!$self->{TEST}) {
+        YAML::DumpFile($self->{CONF}, $self->{YAML});
+    }
+}
+
+sub run {
+    my ($self) = @_;
+    
+    $self->_read_config() if(!defined($self->{YAML}));
+    
+    my $default_command = undef;
+    my $path = undef;
+    if (exists($self->{YAML}->{default}->{command}) && $self->{YAML}->{default}->{command}) {
+        $default_command = $self->{YAML}->{default}->{command};
+    }
+    
+    my $default_path = undef;
+    if (exists($self->{YAML}->{default}->{path}) && $self->{YAML}->{default}->{path}) {
+        $default_path = $self->{YAML}->{default}->{path};
+    }
+    
+    my ($last, $item, $command);
+    my $rss;
+    while(my ($rss, $v) = each(%{$self->{YAML}->{rss}})) {
+        if (exists($v->{url})) {
+            $last = (exists($v->{last}) ? $v->{last} : undef);
+            $command = (exists($v->{command}) ? $v->{command} : $default_command);
+            $path = (exists($v->{path}) ? $v->{path} : $default_path);
+            
+            $rss = $self->_parse_rss($v->{url}, $last);
+            foreach $item (@{$rss->{items}}) {
+                if (defined($path)) {
+                    $self->_save_in_path($item->{link}, $path);
+                } 
+
+                if (defined($command)) {
+                    $command = $self->_create_command($command, $item);
+                    
+                    if ($self->{TEST}) {
+                        print "$command\n";
+                    } else {
+                        system($command);
+                        if ($? != 0) {
+                            carp(sprintf("Commdand '$command' exited with a value '%d'", $? >>8));
+                        }
+                    }
+                }
+            }
+            
+            $v->{last} = $rss->{pubDate};
+        }
+    }
+}
+
+sub _read_config {
+    my ($self) = @_;
+    
+    if (!defined($self->{CONF})) {
+        croak("configuration file is not defined");
+    }
+    
+    if (!-r $self->{CONF}) {
+        croak("file '$self->{CONF}' cannot be read")
+    }
+    
+    my $conf = YAML::LoadFile($self->{CONF});
     if (!exists($conf->{rss})) {
-        if ($DEBUG) {
-            print "Nothing to do\n";
+        if ($self->{DEBUG}) {
+            carp("Nothing to do");
+            print "mariano\n";
             return undef;
         }
     }
 
-    return $conf;
+    $self->{YAML} = $conf;
 }
 
-sub rfc822_to_localtime($) {
-    my $date = shift;
+sub _rfc822_to_localtime {
+    my ($self, $date) = @_;
     return HTTP::Date::str2time($date);
 }
 
-sub parse_rss($$) {
-    my ($url, $last) = @_;
+sub _create_command {
+    my ($self, $cmd, $params) = @_;
+
+    while (my ($k, $v) = each (%$params)) {
+        $v =~ s/"/\"/g;
+        $cmd =~ s/<$k>/"$v"/g;
+    }
+
+    return $cmd;
+}
+
+sub _parse_rss {
+    my ($self, $url, $last) = @_;
     
-    my $tlast = ($last ? rfc822_to_localtime($last) : undef);
+    my $tlast = ($last ? $self->_rfc822_to_localtime($last) : undef);
     my $new_tlast = $tlast;
     my $new_last = $last;
 
@@ -44,17 +297,27 @@ sub parse_rss($$) {
         return ();
     }
     
-    my @links = ();
-    my ($link, $pub_date, $tpub_date, $node);
+    my @items = ();
+    my @children;
+    my ($i, $link, $pub_date, $tpub_date, $node);
     foreach my $item ($feed->query('//item') ) { 
         $node = $item->query('link');
         $link = $node->text_content;
         $node = $item->query('pubDate');
         $pub_date = $node->text_content;
-        $tpub_date = rfc822_to_localtime($pub_date);
+        $tpub_date = $self->_rfc822_to_localtime($pub_date);
         
         if (!$tlast || $tlast < $tpub_date) {
-            push @links, $link;
+            @children = $item->contents();
+            $i = {};
+            
+            foreach (@{$children[0]}) {
+                if ($_->qname) {
+                    $i->{$_->qname} = $_->text_content;
+                }
+            }
+            
+            push @items, $i;
             
             if ($tpub_date > $new_tlast) {
                 $new_tlast = $tpub_date;
@@ -62,73 +325,13 @@ sub parse_rss($$) {
             }
         }
     }
-    
-    my %ret = (links => \@links,
-               new_last => $new_last);
+    my %ret = (items   => \@items,
+               pubDate => $new_last);
     return \%ret;
 }
 
-sub save_in_path($$) {
-    my ($url, $path) = @_;
-    print "save in path $url $path\n";
+sub _save_in_path {
+    my ($self, $link, $path) = @_;
     # TODO
+    carp("Not implemented (link: $link, path: $path)");
 }
-
-sub do_rss_work($) {
-    my $conf = shift;
-    
-    my $command = undef;
-    my $path = undef;
-    if (exists($conf->{default}->{command}) && $conf->{default}->{command}) {
-        $command = $conf->{default}->{command};
-    }
-    
-    if (exists($conf->{default}->{path}) && $conf->{default}->{path}) {
-        $path = $conf->{default}->{path};
-    }
-    
-    my ($last, $link, $cmd);
-    my $rss;
-    while(my ($rss, $v) = each(%{$conf->{rss}})) {
-        if (exists($v->{url})) {
-            $last = (exists($v->{last}) ? $v->{last} : undef);
-            $rss = parse_rss($v->{url}, $last);
-            
-            foreach $link (@{$rss->{links}}) {
-                if (defined($path)) {
-                    save_in_path($link, $path);
-                } 
-
-                if (defined($command)) {
-                    $cmd = $command;
-                    $cmd =~ s/<url>/"$link"/g;
-                    system($cmd);
-                }
-            }
-            
-            $v->{last} = $rss->{new_last};
-        }
-    }
-}
-
-sub main {
-    my $conf_file = $ENV{HOME} . '/.rssd.yml';
-    
-    GetOptions ('conf=s' => \$conf_file,
-                'debug'  => \$DEBUG);
-    
-    my $lock_file = $conf_file . '.lck';
-    
-    open (my $fl, ">> $lock_file") or die ("Error: cannot open $lock_file");
-    flock($fl, LOCK_EX) or die ("Error: cannot lock $lock_file");
-    
-    my $conf = read_config($conf_file);
-    do_rss_work($conf);
-    YAML::DumpFile($conf_file, $conf);
-
-    unlink($lock_file);
-    flock($fl, LOCK_UN) or die ("Error: cannot unlock $lock_file");
-    close($fl);
-}
-
-main();
